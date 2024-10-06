651c2fa215bc1a0b17f75c04a312dffeaed49d29
Pradeep Joshi <pradeepjoshi6002@gmail.com>
2024-10-06 22:53:38 +0530

    major code change

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,6 +1,6 @@
 import Menu from "@/components/Menu";
 import Toolbox from "@/components/Toolbox";
-import Board from "@/components/board";
+import Board from "@/components/Board";
 
 export default function Home() {
   return (
